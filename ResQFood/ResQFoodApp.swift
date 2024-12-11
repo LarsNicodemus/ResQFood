466bb9b34da7f5aa1b9639ab54ebcfe1560413d5
LarsNicodemus <lars@no-19.net>
2024-12-11 16:07:26 +0100

    Initial Commit

---
+++ ResQFood/ResQFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResQFoodApp.swift
+//  ResQFood
+//
+//  Created by Lars Nicodemus on 11.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ResQFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
